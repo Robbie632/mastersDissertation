4bbaf0214e523edf2c35a8ba1975a82b8abe97f8
Robert Morse <robert.morse@intelligentultrasound.com>
2024-05-27 13:40:47 +0100

    now frontend running on nginx server

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,5 +1,5 @@
 
 //REACT_APP_SERVER_IP set to http://0.0.0.0:5000 for local development
 //  setto ip address of back rpi if deployed on raspberry pi 
-export const ENV_VARS = { REACT_APP_SERVER_IP: "http://0.0.0.0:5000" };
+export const ENV_VARS = { REACT_APP_SERVER_IP: "http://0.0.0.0:1337" };
 
