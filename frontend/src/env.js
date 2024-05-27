6ff72ada3c0ad3dfd303f08e54dde0eb38b41e3e
Robert Morse <robert.morse@intelligentultrasound.com>
2024-05-27 12:09:17 +0100

    fixed url path in frontend

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,5 +1,5 @@
 
-//REACT_APP_SERVER_IP set to 0.0.0.0 for local development
+//REACT_APP_SERVER_IP set to http://0.0.0.0:5000 for local development
 //  setto ip address of back rpi if deployed on raspberry pi 
-export const ENV_VARS = { REACT_APP_SERVER_IP: "0.0.0.0" };
+export const ENV_VARS = { REACT_APP_SERVER_IP: "http://0.0.0.0:5000" };
 
