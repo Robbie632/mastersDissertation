89c9e73c9f6a49662d904f61fbf01b9a8063e822
robbie <rob.643@hotmail.co.uk>
2024-10-11 11:45:41 +0100

    improved categories aestehtics and added start learning button

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -6,7 +6,10 @@ export default function Category({ name, setLesson, setEdit }) {
   return (
     <div className="category Holiday-Cheer-2-hex heading-3">
       <div id="category-container-1" >
-        <div id="category-name" onClick={() => setLesson(name)}>{name}</div>
+        <div id="category-name" >{name}</div>
+        <div id="category-start" onClick={() => setLesson(name)} >
+          <div id="category-start-text">START LEARNING</div>
+        </div>
         <div id="category-icons">
           <div className="edit-icon" onClick={() => {
             setEdit(name);
